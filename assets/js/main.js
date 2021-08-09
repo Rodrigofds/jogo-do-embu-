c52de736cef21ee90f307d21b463685fa3dcc884
Rodrigo Ferreira <rodrigoldiscool@gmail.com>
2021-08-09 16:02:55 -0300

    criado o imbuá

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("imbua");
 let context = canvas.getContext("2d");
 let box = 32;
+let imbua = [];
+imbua[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarImbua(){
+    for(i = 0; i < imbua.length; i++){
+        context.fillStyle = "brown";
+        context.fillRect(imbua[i].x, imbua[i].y, box, box);
+    }
+}
+
 criarBG();
+criarImbua();
