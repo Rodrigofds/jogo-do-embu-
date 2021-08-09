8c46ee2f564f9c8b44b9219a1e4f8b943227790a
Rodrigo Ferreira <rodrigoldiscool@gmail.com>
2021-08-09 16:16:43 -0300

    definido movimentos do imbuá

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,8 @@ imbua[0] = {
     y: 8 * box
 }
 
+let direction = "right";
+
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -19,5 +21,28 @@ function criarImbua(){
     }
 }
 
-criarBG();
-criarImbua();
+function iniciarJogo(){
+    criarBG();
+    criarImbua();
+
+    let imbuaX = imbua[0].x;
+    let imbuaY = imbua[0].y;
+
+    if(direction == "right") imbuaX += box;
+    if(direction == "left") imbuaX -= box;
+    if(direction == "up") imbuaY -= box;
+    if(direction == "down") imbuaY += box;
+
+    imbua.pop();
+
+    let newHead = {
+        x: imbuaX,
+        y: imbuaY
+    }
+
+    imbua.unshift(newHead);
+    
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
